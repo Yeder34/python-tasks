2feb23fd97968bf2fae3bd7da59adf4156baa331
Yeder34 <50651976+Yeder34@users.noreply.github.com>
2019-09-09 15:23:50 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,6 @@
+PI = 3.14
+radius = float(input(' Please Enter the radius of a circle: '))
+area = PI * radius * radius
+
+
+print(" Area Of a Circle = %.2f" %area)
---
+++ task8.py
@@ -0,0 +1,6 @@
+def multiply(numbers):  
+    total = 1
+    for n in numbers:
+        total *= n  
+    return total  
+print(multiply((8, 2, 3, -1, 7)))
